cb6b46280250b07bd3fc48b93975e010b92a48f2
natha-ui <136472669+natha-ui@users.noreply.github.com>
2026-02-09 23:09:23 +0000

    Refactor imports in kitti_data.py
    Removed unused imports and added numpy import.

--- kitti_data.py
+++ kitti_data.py
@@ -1,9 +1,6 @@
-import torch
-import torch.utils.data as data
-
-
-
+import numpy as np
 import h5py
+from torch.utils.data import Dataset
 
 class KITTI(Dataset):
     def __init__(self, datafile, sourcefile, nt):
